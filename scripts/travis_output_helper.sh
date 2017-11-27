4df7a05abdf6aa6d4e3765be81235184a284d79a
JensDiemer <git@jensdiemer.de>
2017-11-27 11:17:46 +0100

    fix travis_output_helper.sh

--- scripts/travis_output_helper.sh
+++ scripts/travis_output_helper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# https://stackoverflow.com/a/26082445/185510
+# based on: https://stackoverflow.com/a/26082445/185510
 
 export PING_SLEEP=20s
 export WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -8,21 +8,23 @@ export BUILD_OUTPUT=$WORKDIR/build.out
 
 touch $BUILD_OUTPUT
 
-dump_output() {
-   echo Tailing the last 500 lines of output:
-   tail -500 $BUILD_OUTPUT
+end_output_loop() {
+    echo Tailing the last 500 lines of output:
+    tail -500 $BUILD_OUTPUT
+    kill $PING_LOOP_PID
+    exit 0
 }
 error_handler() {
-  echo ERROR: An error was encountered with the build.
-  dump_output
-  kill $PING_LOOP_PID
-  exit 1
+    echo ERROR: An error was encountered with the build.
+    dump_output
+    kill $PING_LOOP_PID
+    exit 1
 }
 # if an error occurs, run our error handler to output a tail of the build
 trap 'error_handler' ERR
 
-ping_loop() {
-  # set up a repeating loop to send some output to Travis.
-  bash -c "while true; do echo -e \"\\n\\n\$(date) - building ...\"; tail -5 $BUILD_OUTPUT; sleep $PING_SLEEP; done" &
-  PING_LOOP_PID=$!
+start_output_loop() {
+    # set up a repeating loop to send some output to Travis.
+    bash -c "while true; do echo -e \"\\n\\n\$(date) - building ...\"; tail -5 $BUILD_OUTPUT; sleep $PING_SLEEP; done" &
+    PING_LOOP_PID=$!
 }
